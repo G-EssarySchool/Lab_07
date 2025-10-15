c9343db1ee9c66393e0070f6225bdc0e34e68461
G-EssarySchool <essary.619432@swl.k12.oh.us>
2025-10-15 08:55:53 -0400

    task 1

--- src/Task1.java
+++ src/Task1.java
@@ -1,27 +1,49 @@
 import java.util.Scanner;
 
-public class Main {
+public class Task1 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int currentGrade;
-        boolean notDone = true;
-        int highestGrade = -1;
 
-        System.out.println("Please enter a number [0 - 100] [-1 to stop]:");
-        
-        while (notDone) {
-            currentGrade = scan.nextInt();
-            if (currentGrade >= 0 && currentGrade <= 100) {
-                if (currentGrade < highestGrade);
-                highestGrade = currentGrade;
-            }
-            else if (currentGrade == -1){
-                notDone = false;
-            }
-            else {
-                System.out.println("Invalid test grade.");
-            }
+        for(int i = 0; i < 30; i++) {
+            System.out.print("1: ");
         }
-        System.out.println("Your highest test score was : " + highestGrade);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
