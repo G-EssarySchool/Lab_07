793899f2b6d26a28d66cc9beb0db24123c42bc55
G-EssarySchool <essary.619432@swl.k12.oh.us>
2025-10-16 08:34:24 -0400

    Complete Task 1

--- src/Task1.java
+++ src/Task1.java
@@ -4,46 +4,23 @@ public class Task1 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        for(int i = 0; i < 30; i++) {
-            System.out.print("1: ");
+        System.out.println("Example 1:");
+        for(int i = 0; i <= 30; i++) {
+            System.out.print(i + " ");
         }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        System.out.println("\nExample 2:");
+        for(int i = 30; i >= 0; i--) {
+            System.out.print(i + " ");
+        }
+        System.out.println("\nExample 3:");
+        for(int i = 0; i <= 18; i++) {
+            System.out.print(i + " ");
+            i += 2;
+        }
+        System.out.println("\nExample 4:");
+        for(int i = 10; i >= 0; i--) {
+            System.out.print(i + " ");
+            i += -1;
+        }
     }
 }
